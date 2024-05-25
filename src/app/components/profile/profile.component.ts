6097f63cc03daeb53ae280ea5eb09027b2827b26
youssef khalifi <youssefkhalifi767@gmail.com>
2024-05-25 21:58:12 +0100

    create profile of user

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,21 +1,29 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../service/user.service";
 import {_User} from "../../entities/_User";
+import {FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit{
 
   user : _User | undefined;
+
+  ngOnInit(): void {
+    this.user = this.service.user
+  }
   constructor(private service : UserService) {
   }
 
-  ngOnInit(): void {
-        this.user = this.service.user
-    }
+  onSubmit(form : NgForm) {
+    console.log(form.value)
+  }
 }
