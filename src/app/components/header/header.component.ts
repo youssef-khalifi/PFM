a238b993b16390af00af7d17c294976e5a53819b
youssef khalifi <youssefkhalifi767@gmail.com>
2024-05-27 22:51:33 +0100

    change the header add ngif for the admin icons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,19 +3,22 @@ import {Router, RouterLink} from "@angular/router";
 import {UserService} from "../../service/user.service";
 import {CartService} from "../../service/cart.service";
 import {Cart} from "../../entities/Cart";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
-  constructor(public authService: UserService,private router : Router,
+  constructor(public authService: UserService,
+              private router : Router,
               private service : CartService) {
   }
 
--- src/app/entities/Role.ts
+++ src/app/entities/Role.ts
@@ -1,5 +1,5 @@
 export enum Role {
-  Admin = "Admin",
-  User = "User",
-  SuperAdmin = "SuperAdmin",
+  Admin = "ADMIN",
+  User = "USER",
+
 }
